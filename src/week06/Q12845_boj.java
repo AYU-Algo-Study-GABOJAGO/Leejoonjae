98ce99663233b02a9c98991ce325f235116a33f9
leejunjae00 <wnswo8633@naver.com>
2024-05-13 23:22:12 +0900

    Q12845 풀이

---
+++ src/week06/Q12845_boj.java
@@ -0,0 +1,30 @@
+package week06;
+
+import java.util.Scanner;
+import java.util.*;
+import java.util.Arrays;
+
+public class Q12845_boj{
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    Integer[] arr = new Integer[n];
+
+    for(int i = 0; i < n; i++){
+        arr[i] = sc.nextInt();
+    }
+    Arrays.sort(arr,Comparator.reverseOrder());
+
+    int sum = 0;
+    int result = arr[0];
+    for(int j = 1; j < n; j++){
+      sum += result + arr[j];
+
+    }
+    System.out.println(sum);
+
+    }
+  }
