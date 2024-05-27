a1ec2e8a68783cbdf04973619c83766b69e9c15b
leejunjae00 <wnswo8633@naver.com>
2024-05-27 20:56:26 +0900

    Q11659 풀이

--- src/week08/Q11659_boj.java
+++ src/week08/Q11659_boj.java
@@ -3,23 +3,22 @@ package week08;
 import java.util.Scanner;
 
 public class Q11659_boj {
-  public static void main(String[] args) {
+  public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
 
     int n = sc.nextInt();
     int m = sc.nextInt();
 
     int[] arr = new int[n + 1];
-
-    for (int i = 1; i <= n; i++) {
-      arr[i] = arr[i - 1] + sc.nextInt();
+    arr[0] = 0;
+    for (int i = 0; i < n; i++) {
+      arr[i + 1] = arr[i] + sc.nextInt();
     }
 
-    int a,b;
     for (int i = 0; i < m; i++) {
-     a = sc.nextInt();
-     b = sc.nextInt();
-      System.out.println(arr[b] + arr[a - 1]);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      System.out.println(arr[b] - arr[a - 1]);
     }
   }
 }
