86fcc9e92dfade95ca7e1503af17597ca33fb295
leejunjae00 <wnswo8633@naver.com>
2024-06-08 22:50:23 +0900

    Q11687 풀이

---
+++ src/week10/Q2018_boj.java
@@ -0,0 +1,28 @@
+package week10;
+
+import java.util.*;
+public class Q2018_boj {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int total = 0;
+
+    int start = 1;
+    int end = 1;
+    int sum = 1;
+
+    while(start <= end){
+      if(sum == N) total++;
+      if(sum < N){
+        end++;
+        sum += end;
+      }
+      else if(sum >= N){
+        sum -= start;
+        start++;
+      }
+    }
+    System.out.println(total);
+  }
+}
